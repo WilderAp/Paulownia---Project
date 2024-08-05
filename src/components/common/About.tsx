f1492f4cb5ced46025ffc0540ca51ba029f650d1
francorinque <francoagustinrinquecorreo@gmail.com>
2024-08-05 13:04:53 -0300

    Fix(Hero,Advantages): fix styles in sections

--- src/components/common/About.tsx
+++ src/components/common/About.tsx
@@ -1,14 +1,14 @@
-import { Layout } from './Layout';
+import { Layout } from './Layout'
 
 function About() {
   return (
-    <div className='bg-primary min-h-[352px] flex items-center'>
+    <div className="bg-primary min-h-[352px] flex items-center">
       {/* Mobile */}
-      <section className='text-white block sm:hidden p-4'>
-        <h1 className='font-bold mx-auto' style={{ fontFamily: 'Raleway' }}>
+      <section className="text-white block sm:hidden p-4">
+        <h1 className="font-bold mx-auto" style={{ fontFamily: 'Raleway' }}>
           Sobre este proyecto
         </h1>
-        <div className='flex flex-col gap-4 pt-4'>
+        <div className="flex flex-col gap-4 pt-4">
           <p>
             Este proyecto se distingue por ser un cultivo innovador que fusiona
             de manera única la naturaleza y la tecnología para generar un
@@ -25,16 +25,16 @@ function About() {
       </section>
       {/* Desktop */}
       <Layout>
-        <section className='sm:grid md:grid-cols-2 text-white items-center hidden'>
-          <div className='flex justify-between'>
-            <h1 className='text-2xl' style={{ fontFamily: 'Raleway' }}>
+        <section className="sm:grid md:grid-cols-2 text-white items-center hidden">
+          <div className="flex justify-between">
+            <h1 className="text-2xl" style={{ fontFamily: 'Raleway' }}>
               Sobre este proyecto
             </h1>
           </div>
 
-          <div className='flex justify-between gap-6  max-w-[700px] '>
-            <span className='bg-white mr-10' style={{ width: '2px' }} />
-            <div className=''>
+          <div className="flex justify-between gap-6  max-w-[700px] ">
+            <span className="bg-white mr-10" style={{ width: '2px' }} />
+            <div className="">
               <p>
                 Este proyecto se distingue por ser un cultivo innovador que
                 fusiona de manera única la naturaleza y la tecnología para
@@ -53,6 +53,6 @@ function About() {
         </section>
       </Layout>
     </div>
-  );
+  )
 }
-export default About;
+export default About
