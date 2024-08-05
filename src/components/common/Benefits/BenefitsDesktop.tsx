ea4bf874e8a1148d2c172a28050968b1b1a93f38
francorinque <francoagustinrinquecorreo@gmail.com>
2024-08-05 16:09:28 -0300

    Feat(Benefits): add section benefits, firts part

--- src/components/common/Benefits/BenefitsDesktop.tsx
+++ src/components/common/Benefits/BenefitsDesktop.tsx
@@ -1,11 +1,11 @@
-import { Layout } from '../Layout';
-import { FaCheck } from 'react-icons/fa';
+import { FaCheck } from 'react-icons/fa'
+import { Layout } from '../Layout'
 
 const images = [
   './pictures/benefits/desktop/one.jfif',
   './pictures/benefits/desktop/two.png',
   './pictures/benefits/desktop/three.jfif',
-];
+]
 
 const list = [
   'Empleo en las comunidades locales',
@@ -14,42 +14,42 @@ const list = [
   'Alimento para ganado',
   'Madera de alta calidad',
   'Cosmecéutica y perfumería natural',
-];
+]
 
 const BenefitsDesktop = () => {
   return (
-    <section className='hidden xl:block bg-bgSection py-[64px]'>
+    <section className="hidden xl:block bg-bgSection py-[64px]">
       <Layout>
         {/* header */}
-        <div className='flex  w-full items-center justify-center gap-[24px]'>
-          <h2 className='text-[36px] font-raleway font-bold text-title'>
+        <div className="flex  w-full items-center justify-center gap-[24px]">
+          <h2 className="text-[36px] font-raleway font-bold text-title">
             Beneficios que obtenemos
           </h2>
-          <div className='h-[117px] w-[1px] bg-primary' />
-          <p className='w-[541px]'>
+          <div className="h-[117px] w-[1px] bg-primary" />
+          <p className="w-[541px]">
             La plantación de Paulownia nos reporta directamente beneficios que
             afectan positivamente en nuestra vida. Con notas de sostenibilidad,
             desarrollo económico, avances tecnológicos y de manera ecológica.
           </p>
         </div>
         {/* container list and images */}
-        <div className='mt-[90px] flex gap-10 items-center w-full h-[520px]'>
-          <ul className='min-w-[400px] max-w-[400px] h-full flex flex-col justify-around'>
+        <div className="mt-[90px] flex gap-10 items-center w-full h-[520px]">
+          <ul className="min-w-[400px] max-w-[400px] h-full flex flex-col justify-around">
             {list.map((item) => (
               <li
                 key={crypto.randomUUID()}
-                className='text-[14px] font-raleway font-bold text-title flex items-center gap-2'
+                className="text-[14px] font-raleway font-bold text-title flex items-center gap-2"
               >
-                <FaCheck className='text-primary text-[20px]' />
+                <FaCheck className="text-primary text-[20px]" />
                 {item}
               </li>
             ))}
           </ul>
-          <div className='w-full h-full flex gap-4 relative '>
+          <div className="w-full h-full flex gap-4 relative ">
             {images.map((img, index) => (
               <img
                 src={img}
-                alt='benefits'
+                alt="benefits"
                 key={crypto.randomUUID()}
                 className={`w-[33%] h-full object-cover rounded-[32px] ${
                   index === 1 ? 'blur-[3px]' : ''
@@ -57,15 +57,15 @@ const BenefitsDesktop = () => {
               />
             ))}
             <img
-              src='./pictures/benefits/desktop/two-plant-png.png'
-              alt='benefits'
-              className='absolute mx-auto bottom-[-2px] left-[-25px] z-2   object-cover'
+              src="./pictures/benefits/desktop/two-plant-png.png"
+              alt="benefits"
+              className="absolute mx-auto bottom-[-2px] left-[-25px] z-2   object-cover"
             />
             ;
           </div>
         </div>
       </Layout>
     </section>
-  );
-};
-export default BenefitsDesktop;
+  )
+}
+export default BenefitsDesktop
--- src/components/common/Benefits/BenefitsMobile.tsx
+++ src/components/common/Benefits/BenefitsMobile.tsx
@@ -1,5 +1,5 @@
-import { Layout } from '../Layout';
-import { FaCheck } from 'react-icons/fa';
+import { FaCheck } from 'react-icons/fa'
+import { Layout } from '../Layout'
 
 const items = [
   'Empleo en las comunidades locales',
@@ -8,27 +8,27 @@ const items = [
   'Alimento para ganado',
   'Cosmecéutica y perfumería natural',
   'Madera de alta calidad',
-];
+]
 
 const BenefitsMobile = () => {
   return (
-    <section className='xl:hidden  py-[64px]'>
+    <section className="xl:hidden  py-[64px]">
       <Layout>
         {/* container image , texts and list */}
-        <div className='flex flex-wrap'>
-          <div className='mx-auto  '>
-            <figure className='w-[396px] h-[204px]'>
+        <div className="flex flex-wrap">
+          <div className="mx-auto  ">
+            <figure className="w-[396px] h-[204px]">
               <img
-                src='/pictures/benefits/mobile/one.png'
-                alt='a'
-                className='rounded-[16px]  w-full h-full object-cover'
+                src="/pictures/benefits/mobile/one.png"
+                alt="a"
+                className="rounded-[16px]  w-full h-full object-cover"
               />
             </figure>
-            <div className='w-[430px] mx-auto py-[12px] pr-[24px] pl-[16px] flex flex-col gap-[16px] text-customGrey'>
-              <h2 className='text-[16px] font-raleway font-bold '>
+            <div className="w-[430px] mx-auto py-[12px] pr-[24px] pl-[16px] flex flex-col gap-[16px] text-customGrey">
+              <h2 className="text-[16px] font-raleway font-bold ">
                 Beneficios que obtenemos
               </h2>
-              <p className='font-normal text-[14px]  font-ptSans'>
+              <p className="font-normal text-[14px]  font-ptSans">
                 La plantación de Paulownia nos reporta directamente beneficios
                 que afectan positivamente en nuestra vida. Con notas de
                 sostenibilidad, desarrollo económico, avances tecnológicos y de
@@ -36,13 +36,13 @@ const BenefitsMobile = () => {
               </p>
             </div>
           </div>
-          <ul className='w-[398px] mx-auto flex flex-col gap-[20px] mt-4 '>
+          <ul className="w-[398px] mx-auto flex flex-col gap-[20px] mt-4 ">
             {items.map((item) => (
               <li
                 key={crypto.randomUUID()}
-                className='flex gap-2 items-center text-[12px] font-ptSans'
+                className="flex gap-2 items-center text-[12px] font-ptSans"
               >
-                <FaCheck className='text-primary text-[20px]' />
+                <FaCheck className="text-primary text-[20px]" />
                 {item}
               </li>
             ))}
@@ -50,18 +50,18 @@ const BenefitsMobile = () => {
         </div>
 
         {/* container images and text */}
-        <div className='w-full mx-auto mt-8 flex flex-col md:flex-row justify-between gap-2'>
-          <div className='flex flex-col gap-5 w-[398px] mx-auto md:mx-0 md:w-[50%]'>
-            <figure className='h-[500px] w-full'>
+        <div className="w-full mx-auto mt-8 flex flex-col md:flex-row justify-between gap-2">
+          <div className="flex flex-col gap-5 w-[398px] mx-auto md:mx-0 md:w-[50%]">
+            <figure className="h-[500px] w-full">
               <img
-                src='/pictures/benefits/mobile/two.jfif'
-                alt='b'
-                className='rounded-[16px]  w-full h-full object-cover'
+                src="/pictures/benefits/mobile/two.jfif"
+                alt="b"
+                className="rounded-[16px]  w-full h-full object-cover"
               />
             </figure>
-            <div className='bg-[#F8F8F8]  rounded-[16px] py-[32px] px-[30px] h-[230px] grid place-content-center'>
-              <h3 className='font-bold font-raleway text-[16px]'>Visión</h3>
-              <p className='font-normal font-ptSans text-[14px] mt-5'>
+            <div className="bg-[#F8F8F8]  rounded-[16px] py-[32px] px-[30px] h-[230px] grid place-content-center">
+              <h3 className="font-bold font-raleway text-[16px]">Visión</h3>
+              <p className="font-normal font-ptSans text-[14px] mt-5">
                 Combinar innovación, naturaleza y sostenibilidad para generar un
                 impacto positivo ambiental y socioeconómico de manera sencilla,
                 accesible y cercana, que implique al mayor número de agentes
@@ -69,17 +69,17 @@ const BenefitsMobile = () => {
               </p>
             </div>
           </div>
-          <div className='flex flex-col w-[398px] gap-5 mx-auto md:mx-0 md:w-[50%]'>
-            <figure className='h-[500px] w-full'>
+          <div className="flex flex-col w-[398px] gap-5 mx-auto md:mx-0 md:w-[50%]">
+            <figure className="h-[500px] w-full">
               <img
-                src='/pictures/benefits/mobile/three.jfif'
-                alt='b'
-                className='rounded-[16px] w-full h-full object-cover'
+                src="/pictures/benefits/mobile/three.jfif"
+                alt="b"
+                className="rounded-[16px] w-full h-full object-cover"
               />
             </figure>
-            <div className='bg-[#F8F8F8]  rounded-[16px] py-[32px] px-[30px] h-[230px] grid place-content-center'>
-              <h3 className='font-bold font-raleway text-[16px]'>Misión</h3>
-              <p className='font-normal font-ptSans text-[14px] mt-5'>
+            <div className="bg-[#F8F8F8]  rounded-[16px] py-[32px] px-[30px] h-[230px] grid place-content-center">
+              <h3 className="font-bold font-raleway text-[16px]">Misión</h3>
+              <p className="font-normal font-ptSans text-[14px] mt-5">
                 Aportamos un valor diferencial en la industria de la madera
                 sostenible y la cosmética natural, a través del equilibrio entre
                 tecnología avanzada y recursos naturales. Lideramos un impacto
@@ -91,6 +91,6 @@ const BenefitsMobile = () => {
         </div>
       </Layout>
     </section>
-  );
-};
-export default BenefitsMobile;
+  )
+}
+export default BenefitsMobile
