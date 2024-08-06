227fdcac5afc73302b4cf8748d9ca117186fe7d8
francorinque <francoagustinrinquecorreo@gmail.com>
2024-08-06 19:15:52 -0300

    resolve conflicts

--- src/components/common/ScrollToTop.tsx
+++ src/components/common/ScrollToTop.tsx
@@ -1,40 +1,40 @@
-import { useEffect, useState } from 'react';
-import { IoIosArrowUp } from 'react-icons/io';
+import { useEffect, useState } from 'react'
+import { IoIosArrowUp } from 'react-icons/io'
 
 const ScrollToTop = () => {
-  const [scrolled, setScrolled] = useState(false);
+  const [scrolled, setScrolled] = useState(false)
 
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
       behavior: 'smooth',
-    });
-  };
+    })
+  }
 
   useEffect(() => {
     const onScroll = () => {
       if (window.scrollY > 100) {
-        setScrolled(true);
+        setScrolled(true)
       } else {
-        setScrolled(false);
+        setScrolled(false)
       }
-    };
+    }
 
-    window.addEventListener('scroll', onScroll);
+    window.addEventListener('scroll', onScroll)
 
-    return () => window.removeEventListener('scroll', onScroll);
-  }, []);
+    return () => window.removeEventListener('scroll', onScroll)
+  }, [])
 
   return (
     <button
-      type='button'
+      type="button"
       className={`btn bg-black text-white btn-icon fixed bottom-[3vh] right-[3vw] ${
         scrolled ? 'opacity-100' : 'opacity-0'
       }`}
       onClick={scrollToTop}
     >
-      <IoIosArrowUp size='24px' />
+      <IoIosArrowUp size="24px" />
     </button>
-  );
-};
-export default ScrollToTop;
+  )
+}
+export default ScrollToTop
