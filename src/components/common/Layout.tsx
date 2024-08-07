5b3239c828797418b47375d36be6080be46a317e
francorinque <francoagustinrinquecorreo@gmail.com>
2024-08-07 10:38:53 -0300

    Fix(Hero): fix padding

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,17 +1,20 @@
-import type React from 'react'
+import type React from 'react';
+import { useEffect } from 'react';
 
 interface LayoutProps {
-  maxWidth?: string
-  children: React.ReactNode
+  maxWidth?: string;
+  children: React.ReactNode;
 }
 
 export const Layout: React.FC<LayoutProps> = ({
   maxWidth = 'max-w-[1200px]',
   children,
 }) => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
-    <div className={` w-full ${maxWidth} mx-auto px-[16px] py-4`}>
-      {children}
-    </div>
-  )
-}
+    <div className={` w-full ${maxWidth} mx-auto px-[16px]`}>{children}</div>
+  );
+};
